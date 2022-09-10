fccc6a8cb376dd7dbdb45c12fc8d57ebab23b493
Michał Zaręba <michalzareba0001@gmail.com>
2022-09-10 20:45:31 +0200

    js form added

--- banery.ts
+++ banery.ts
@@ -1,4 +1,19 @@
 
+let changeText = () => {
+    var text1 = document.getElementById('baner_text_i1').value;
+    var text2 = document.getElementById('baner_text_i2').value;
+    var banerColor = document.getElementById('baner_color').value;
+    
+    document.getElementById('text_out1').innerHTML = text1;
+    document.getElementById('text_out2').innerHTML = text2;
+
+    console.log(text1);
+    console.log(text2);
+}
+
+
+
+
 
 //https://redstapler.co/create-pdf-javascript/
 //https://www.npmjs.com/package/jspdf
