3581d1062ea068f8973ac8f781e0e85dfb49a41e
Michał Zaręba <michalzareba0001@gmail.com>
2022-09-14 20:50:43 +0200

    some angular.js added

--- banery.js
+++ banery.js
@@ -1,17 +1,4 @@
-var changeText = function () {
-    var text1 = document.getElementById('baner_text_i1').value;
-    var text2 = document.getElementById('baner_text_i2').value;
-    var banerColor = document.getElementById('baner_color').value;
-    var textOut1 = document.getElementById('text_out1');
-    var textOut2 = document.getElementById('text_out2');
-
-    document.getElementById('text_out1').innerHTML = text1;
-    document.getElementById('text_out2').innerHTML = text2;
-    document.getElementById('baner_image').style.backgroundColor = banerColor;
-
-
-    console.log(text1);
-    console.log(text2);
+var changeSize = function () {
     var checkSize = document.getElementsByName('baner_size');
     for (var i = 0; i < checkSize.length; i++) {
         if (checkSize[i].checked) {
@@ -19,7 +6,6 @@ var changeText = function () {
         }
     }
 
-    console.log(banerSize);
     if (banerSize == '100100') {
         document.getElementById('baner_image').style.width = '500px';
         document.getElementById('baner_image').style.height = '500px';
@@ -64,5 +50,3 @@ function generateJpeg() {
 
 
 
-//https://redstapler.co/create-pdf-javascript/
-//https://www.npmjs.com/package/jspdf
