133c7339b005152f374beaadac5ec21d3095215e
Michał Zaręba <michalzareba0001@gmail.com>
2022-09-19 18:10:58 +0200

    font size changer

--- banery.js
+++ banery.js
@@ -1,6 +1,46 @@
-function changeBanerSize(x, y){
+var fontSize1 = 4;
+var fontSize2 = 4;
+
+
+function changeBanerSize(x, y) {
     document.getElementById('baner_image').style.width = x + 'px';
     document.getElementById('baner_image').style.height = y + 'px';
+}
+
+function textSize1Plus() {
+    fontSize1++;
+    if (fontSize1 > 8) {
+        fontSize1 = 8;
+        return;
+    }
+    document.getElementById('text_out1').style.fontSize = fontSize1 + 'rem';
+}
+
+function textSize1Minus() {
+    fontSize1--;
+    if (fontSize1 < 1) {
+        fontSize1 = 1;
+        return;
+    }
+    document.getElementById('text_out1').style.fontSize = fontSize1 + 'rem';
+}
+
+function textSize2Plus() {
+    fontSize2++;
+    if (fontSize2 > 8) {
+        fontSize2 = 8;
+        return;
+    }
+    document.getElementById('text_out2').style.fontSize = fontSize2 + 'rem';
+}
+
+function textSize2Minus() {
+    fontSize2--;
+    if (fontSize2 < 1) {
+        fontSize2 = 1;
+        return;
+    }
+    document.getElementById('text_out2').style.fontSize = fontSize2 + 'rem';
 }
 
 function generateJpeg() {
